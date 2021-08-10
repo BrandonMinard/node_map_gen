fc57373a71dafa9e74b87ba924af0c7a747a5639
BrandonMinard <brandon.minard@gmail.com>
2021-08-10 06:45:11 -0600

    added very basic movement, fixed bug where away/towards flipped when x was the same

--- js.js
+++ js.js
@@ -1,29 +1,3 @@
-// const canvas1 = document.getElementById('my-house');
-// const ctx = canvas1.getContext('2d');
-
-// function resizeCanvas(context) {
-//     context.width = window.innerWidth;
-//     context.height = window.innerHeight;
-//     draw()
-// }
-// resizeCanvas(ctx)
-// Set line width
-// ctx.lineWidth = 1;
-
-// Wall
-// ctx.strokeRect(75, 140, 150, 110);
-
-// // Door
-// ctx.fillRect(130, 190, 40, 60);
-
-// // Roof
-// ctx.beginPath();
-// ctx.moveTo(50, 140);
-// ctx.lineTo(150, 60);
-// ctx.lineTo(250, 140);
-// ctx.closePath();
-// ctx.stroke();
-
 //So first things first, figure out where all circles are.
 //They must be at least x+y units away, where x is the radius, and y is the length of the line.
 //I don't know how to optimize this, so why not just try this shit sorta randomly?
@@ -52,73 +26,40 @@ const height = window.innerHeight - 100
 //     draw(context, document.getElementById("firstNum").value, document.getElementById("secondNum").value)
 // }
 //radius probably needs to be more contextual
+
 const radius = 50
-nodeList = ["a", "b", "c", "d"]
-nodes = {
+let nodeList = ["a", "b", "c", "d"]
+let nodes = {
     "a": [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius],
     "b": [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius],
     "c": [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius],
     "d": [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius]
 }
-connections = [["a", "b"], ["b", "c"], ["c", "d"], ["b", "d"]]
-renderNodesAndConnections(context, nodes, connections)
-// let nodeA = [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius];
-// let nodeB = [generateNum(width - (2 * radius)) + radius, generateNum(height - (2 * radius)) + radius];
-// console.log(nodeA, nodeB)
-// console.log(findAngleBetweenNodes(nodeA, nodeB))
-// console.log(findPointFromAngle(findAngleBetweenNodes(nodeA, nodeB), radius))
-// console.log(nodeB)
+let connections = [["a", "b"], ["b", "c"], ["c", "d"], ["b", "d"], ["a", "d"]]
+// renderNodesAndConnections(context, nodes, connections)   
+renderNodesAndConnections(context, nodes, nodeList, connections)
+context.clearRect(0, 0, htmlCanvas.width, htmlCanvas.height)
+// console.log(connections)
+let interval = setInterval(doTick, 16, context, nodeList, nodes, connections);
+function doTick(context, nodeList, nodes, connections) {
+    //mess with nodes here
+    context.clearRect(0, 0, htmlCanvas.width, htmlCanvas.height)
+    nodes["a"] = [nodes["a"][0] + 1, nodes["a"][1]]
+    renderNodesAndConnections(context, nodes, nodeList, connections)
 
-// context.beginPath();
-// context.moveTo(nodeA[0], nodeA[1]);
-// context.lineTo(nodeB[0], nodeB[1]);
-// context.closePath();
-// context.stroke();
+}
 
-// // setInterval(myMethod, 500);
-// context.fillStyle = "white"
-// // function myMethod() {
-// drawCircle(context, nodeA[0], nodeA[1], radius)
-// drawCircle(context, nodeB[0], nodeB[1], radius)
-// let thing = findPointFromRadians(findRadiansBetweenNodes(nodeA, nodeB), radius)
-// context.fillStyle = "blue"
-// if (nodeA[0] > nodeB[0]) {
-//     drawCircle(context, nodeA[0] - thing[0], nodeA[1] - thing[1], 10)
-//     context.fillStyle = "green"
-//     drawCircle(context, nodeA[0] + thing[0], nodeA[1] + thing[1], 10)
-// } else {
-//     drawCircle(context, nodeA[0] + thing[0], nodeA[1] + thing[1], 10)
-//     context.fillStyle = "green"
-//     drawCircle(context, nodeA[0] - thing[0], nodeA[1] - thing[1], 10)
-// }
-// context.fillStyle = "blue"
-// thing = findPointFromRadians(findRadiansBetweenNodes(nodeB, nodeA), radius)
-// if (nodeB[0] > nodeA[0]) {
-//     drawCircle(context, nodeB[0] - thing[0], nodeB[1] - thing[1], 10)
-//     context.fillStyle = "green"
-//     drawCircle(context, nodeB[0] + thing[0], nodeB[1] + thing[1], 10)
-// } else {
-//     drawCircle(context, nodeB[0] + thing[0], nodeB[1] + thing[1], 10)
-//     context.fillStyle = "green"
-//     drawCircle(context, nodeB[0] - thing[0], nodeB[1] - thing[1], 10)
-// }
+function generateMotionVectors(nodes, connections) {
+    //want to return updated nodes
+}
 
 
-// // context.fillStyle = "blue"
-// // drawCircle(context, nodeA[0] + thing[0], nodeA[1] + thing[1], 10)
-// // }
-// // context.fillStyle = "white"
+function stopInterval() {
+    clearInterval(interval)
+}
 
-// context.fillStyle = 'black'
-// context.font = '48px serif'
-// context.textAlign = 'center';
-// context.textBaseline = "middle";
-// drawLetter(context, nodeA[0], nodeA[1], "A")
-// drawLetter(context, nodeB[0], nodeB[1], "B")
+function renderNodesAndConnections(context, nodes, nodeList, connections) {
 
-
-
-function renderNodesAndConnections(context, nodes, connections) {
     //render all the connections, must do this first.
     context.fillStyle = "black"
     context.font = '48px serif'
@@ -164,7 +105,7 @@ function drawCircle(ctx, x, y, r) {
 
 function drawCircleOnNodeRadiansRadius(context, nodes, radians, radius1) {
     context.fillStyle = "blue"
-    if (nodes[0][0] > nodes[1][0]) {
+    if (nodes[0][0] >= nodes[1][0]) {
         drawCircle(context, nodes[0][0] - radians[0], nodes[0][1] - radians[1], radius1)
         context.fillStyle = "green"
         drawCircle(context, nodes[0][0] + radians[0], nodes[0][1] + radians[1], radius1)
